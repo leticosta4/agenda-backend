f314c1c0a2c2953f62d8d18ff6e5679c7cd019ac
Verttb <alyssonoliveirai456@gmail.com>
2025-09-24 00:23:15 -0300

    feat: cors

---
+++ src/main/java/com/engsw/agenda/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.engsw.agenda.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // libera todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // origem do seu frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH","OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
